a3de5644287b976bf496b734f626e23ab2412b55
mateogrosso <mateogrosso04@gmail.com>
2025-12-09 03:06:37 -0300

    CAMBIO QUE AVISA AL FRONT QUE ES SEGURO REALIZAR EL FETCH AL PUERTO 8080

--- Backend/src/main/java/ar/edu/utn/frsf/sistemahotelero/controller/EstadoHabitacionesController.java
+++ Backend/src/main/java/ar/edu/utn/frsf/sistemahotelero/controller/EstadoHabitacionesController.java
@@ -6,9 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.time.LocalDate;
-
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/habitaciones")
 public class EstadoHabitacionesController {
